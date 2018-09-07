30167aa20696be06fcdd8b88e1def3905f41b5a6
Dan Beck <dbeck@axis41.com>
2018-09-06 21:22:55 -0600

    added profile parameter functionality

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -18,14 +18,23 @@ import click
 
 from bucket import BucketManager
 
-SESSION = boto3.Session(profile_name='default')
-bucket_manager = BucketManager(SESSION)
 
+SESSION = None
+bucket_manager = None
 
 @click.group()
-def cli():
+@click.option('--profile', default=None,
+    help="Use a given AWS profile.")
+def cli(profile):
     """Webotron deploys websites to AWS."""
-    pass
+    global SESSION, bucket_manager
+    
+    session_cfg = {}
+    if profile:
+        session_cfg['profile_name'] = profile
+    SESSION = boto3.Session(**session_cfg)
+    bucket_manager = BucketManager(SESSION)
+
 
 
 @cli.command('list-buckets')
